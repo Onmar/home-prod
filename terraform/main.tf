0b9814213772de1f062e7b41788875257c70a937
Onmar <Onmar@users.noreply.github.com>
2025-05-01 14:08:18 +0000

    terraform machine provisioning

---
+++ terraform/main.tf
@@ -0,0 +1,58 @@
+resource "proxmox_vm_qemu" "srv-services" {
+  for_each = local.variables.servers
+
+  vmid             = each.value.vmid
+  name             = each.key
+  desc             = each.value.description
+  target_node      = local.secrets.target_node
+  cores            = 4
+  memory           = 16 * 1024
+  balloon          = 16 * 1024
+  boot             = "order=scsi0;ide2"
+  scsihw           = "virtio-scsi-single"
+  agent            = 1
+  vm_state         = "running"
+  automatic_reboot = true
+
+  nameserver   = local.variables.nameserver
+  searchdomain = local.secrets.domain
+  ipconfig0    = "ip=${each.value.ip}/${local.variables.cidr},gw=${local.variables.gateway}"
+  ciuser       = local.secrets.ci_user
+  cipassword   = local.secrets.ci_password
+  sshkeys      = local.secrets.sshkeys
+
+  serial {
+    id = 0
+  }
+
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          storage = local.variables.disk_storage
+          size    = "128G"
+        }
+      }
+    }
+    ide {
+      ide1 {
+        cloudinit {
+          storage = local.variables.disk_storage
+        }
+      }
+      ide2 {
+        cdrom {
+          iso = "local:iso/talos-v1.10.0-amd64.iso"
+        }
+      }
+    }
+  }
+
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = local.variables.net_bridge
+    tag    = local.variables.net_tag
+    mtu    = 1500  # ToDo: Change to 1 once bug is fixed in RC9
+  }
+}
---
+++ terraform/providers.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc8"
+    }
+
+    sops = {
+      source = "carlpett/sops"
+      version = "1.2.0"
+    }
+  }
+}
+
+provider "sops" {}
+
+data "sops_file" "secrets" {
+  source_file = "secrets.sops.yml"
+}
+
+locals {
+  variables = yamldecode(file("variables.yml"))
+  secrets = data.sops_file.secrets.data
+}
+
+provider "proxmox" {
+  pm_api_url          = local.secrets.prox_api_url
+  pm_api_token_id     = local.secrets.prox_api_token_id
+  pm_api_token_secret = local.secrets.prox_api_token_secret
+  pm_tls_insecure     = local.secrets.prox_tls_insecure
+}
